1326a705e4820485cdaec0ed7cff570fea55871a
CaptM44 <mmm211@zips.uakron.edu>
2016-02-09 01:23:50 -0500

    pre spots

--- Parking Availability  Map/Controllers/ApiController.cs
+++ Parking Availability  Map/Controllers/ApiController.cs
@@ -7,7 +7,17 @@ using System.Web.Mvc;
 namespace Parking_Availability_Map.Controllers
 {
     public class ApiController : Controller
-    {     
+    {
+        public string GetLot()
+        {
+            return readFile(currentLotPath);
+        }
+
+        public string GetLotHistory()
+        {
+            return readFile(historicLotPath);
+        }
+
         public string Update(string lot)
         {
             //random data handler
@@ -24,13 +34,13 @@ namespace Parking_Availability_Map.Controllers
                 }
             }
 
-            //handel hex inputs
+            //handle hex inputs
             if (lot.StartsWith("0x"))
             {
                 try
                 {
                     lot = String.Join(String.Empty, lot.Substring(2).Select(
-                        c => ReverseString(Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0'))
+                        c => reverseString(Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0'))
                     ));
                 }
                 catch { }
@@ -40,28 +50,12 @@ namespace Parking_Availability_Map.Controllers
             var context = GlobalHost.ConnectionManager.GetHubContext<LotHub>();
             context.Clients.All.render(lot);
 
-            //save current lot to disk
+            //save current lot info to disk
             writeFile(currentLotPath, lot);
+            string newHistlotStr = writeHistory(lot);
+            string newAgelotStr = writeAge(lot);
 
-            //read history of lot from disk
-            int[] oldHistLot;
-            try { oldHistLot = readFile(historicLotPath)?.Split(',').Select(x => int.Parse(x)).ToArray(); }
-            catch { oldHistLot = new int[0]; }
-            var newHistlot = new int[Math.Max(oldHistLot.Length, lot.Length)];
-
-            //add cuurent lot data to history
-            for (int i = 0; i < newHistlot.Length; i++)
-            {
-                if (oldHistLot.Length > i)
-                    newHistlot[i] = oldHistLot[i];
-                if (lot.Length > i)
-                    newHistlot[i] += int.Parse(lot[i].ToString());
-            }
-
-            //update history of lot on disk
-            var newHistlotStr = String.Join(",", newHistlot);
-            writeFile(historicLotPath, newHistlotStr);
-            return "Lot History: \n\r" + newHistlotStr;
+            return "Lot Age: \n\r" + newHistlotStr;
         }
 
         public string ClearHistory()
@@ -70,14 +64,23 @@ namespace Parking_Availability_Map.Controllers
             return "Successfully Cleared!";
         }
 
-        public string GetLot()
+        public string ClearAge()
         {
-            return readFile(currentLotPath);
+            writeFile(ageLotPath, "0");
+            return "Successfully Cleared!";
         }
+
+
 
         //----------------------------------------------------------------
 
         //private string path = System.IO.Path.GetTempPath() + @"file.txt";
+
+        private string lotPath
+        {
+            get { return Server.MapPath("~/") + @"lot.txt"; }
+            set { lotPath = value; }
+        }
 
         private string currentLotPath
         {
@@ -90,6 +93,64 @@ namespace Parking_Availability_Map.Controllers
             get { return Server.MapPath("~/") + @"historic_lot.txt"; }
             set { historicLotPath = value; }
         }
+
+        private string ageLotPath
+        {
+            get { return Server.MapPath("~/") + @"age_lot.txt"; }
+            set { ageLotPath = value; }
+        }
+
+        private string writeAge(string lot)
+        {
+            //read age of lot from disk
+            int[] oldAgeLot;
+            try { oldAgeLot = readFile(ageLotPath)?.Split(',').Select(x => int.Parse(x)).ToArray(); }
+            catch { oldAgeLot = new int[0]; }
+            var newAgelot = new int[lot.Length];
+
+            //add current lot data to age
+            for (int i = 0; i < lot.Length; i++)
+            {
+                if (lot[i] == '0')
+                    newAgelot[i] = 0;
+                else
+                {
+                    if (oldAgeLot.Length > i)
+                        newAgelot[i] = oldAgeLot[i] + 1;
+                    else
+                        newAgelot[i] = 0;
+                }
+            }
+
+            //update age of lot on disk
+            var newAgelotStr = String.Join(",", newAgelot);
+            writeFile(ageLotPath, newAgelotStr);
+            return newAgelotStr;
+        }
+
+        private string writeHistory(string lot)
+        {
+            //read history of lot from disk
+            int[] oldHistLot;
+            try { oldHistLot = readFile(historicLotPath)?.Split(',').Select(x => int.Parse(x)).ToArray(); }
+            catch { oldHistLot = new int[0]; }
+            var newHistlot = new int[Math.Max(oldHistLot.Length, lot.Length)];
+
+            //add current lot data to history
+            for (int i = 0; i < newHistlot.Length; i++)
+            {
+                if (oldHistLot.Length > i)
+                    newHistlot[i] = oldHistLot[i];
+                if (lot.Length > i)
+                    newHistlot[i] += int.Parse(lot[i].ToString());
+            }
+
+            //update history of lot on disk
+            var newHistlotStr = String.Join(",", newHistlot);
+            writeFile(historicLotPath, newHistlotStr);
+            return newHistlotStr;
+        }
+
 
         private static string readFile(string path)
         {
@@ -109,7 +170,7 @@ namespace Parking_Availability_Map.Controllers
             }
         }
 
-        private static string ReverseString(string s)
+        private static string reverseString(string s)
         {
             char[] arr = s.ToCharArray();
             Array.Reverse(arr);
---
+++ Parking Availability  Map/Models/ParkingSpot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parking_Availability_Map.Models
+{
+    public class ParkingSpot
+    {
+        public bool IsTaken { get; set; }
+        public int Age { get; set; }
+        public int History { get; set; }
+    }
+}
